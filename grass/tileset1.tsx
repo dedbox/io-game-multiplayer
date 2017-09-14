b1e33ac4ff5c17550026c648b2e7d8388618936d
Eric Griffis <dedbox@gmail.com>
2017-09-14 04:39:40 -0700

    add grass level and Tiled importer plugin

---
+++ grass/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass1" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="tiles1.png" trans="000000" width="640" height="576"/>
+</tileset>
---
+++ grass/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass2" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="tiles2.png" trans="000000" width="968" height="526"/>
+</tileset>
