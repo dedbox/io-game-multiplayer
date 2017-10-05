b2d4ba08782f1359b0a2ae2733a7e78cd4cfcea9
Athan Lee <chrislee@Chriss-MacBook-Air.local>
2017-10-05 18:24:05 -0500

    add tile maps

---
+++ athan/mountain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mountain" tilewidth="16" tileheight="16" tilecount="1920" columns="40">
+ <image source="2dpixx_ccart_dark_mountain_tiles.png" width="640" height="768"/>
+</tileset>
